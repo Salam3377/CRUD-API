bd1aa4ca847f532a659c466f23541fbc8115e494
Salam337 <salamadash@gmail.com>
2022-10-18 23:25:16 -0400

    before delpoy

--- models/connection.js
+++ models/connection.js
@@ -6,13 +6,14 @@ const mongoose = require("mongoose")
 
 // Database connection
 const DATABASE_URL = process.env.DATABASE_URL
+const DEPLOYED_URL = process.env.DEPLOYED_URL
 const CONFIG = {
   useNewUrlParser: true, // need these to connect and run mongoose
   useUnifiedTopology: true,
 }
 
 // Establish connection
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(DEPLOYED_URL, CONFIG)
 
 // tell mongoose what to do with certain events
 mongoose.connection
